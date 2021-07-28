673eb89cb5457f909fef5ae19e342f823ee08860
zero1nine <jeffrey.d.pyle@gmail.com>
2021-07-28 16:14:43 -0700

    added practice code

--- ex11.py
+++ ex11.py
@@ -6,3 +6,10 @@ print("How much do you weigh?", end=' ')
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# Additional practice
+print("How many quarters do you have?", end=' ')
+quarters = int(input())
+print("How many dimes do you have?", end=' ')
+dimes = int(input())
+print(f"You have {.25 * quarters + .1 * dimes} cents.")
