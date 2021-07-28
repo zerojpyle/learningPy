d5a250f315bea49d5e8871f91d63195600e9160d
zero1nine <jeffrey.d.pyle@gmail.com>
2021-07-27 19:15:28 -0700

    added comments

--- ex3.py
+++ ex3.py
@@ -1,23 +1,47 @@
+# Adding comments to describe code
+#
+# This line prints some text
 print("I will now count my chickens:")
 
+# These lines print some text and then the result of some calculations
 print("Hens", 25.0 + 30.0 / 6.0)
 print("Roosters", 100.0 - 25.0 * 3.0 % 4)
 
+# This line prints some text
 print("Now I will count the eggs:")
 
+# This line prints the result of a calculation
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# This line prints some text
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# This line prints the result of a calculation
 print(3 + 2 < 5 - 7)
 
+# These lines print some text and then the result of some calculations
 print("What is 3 + 2?", 3.0 + 2.0)
 print("What is 5 - 7?", 5.0 - 7.0)
 
+# This line prints some text
 print("Oh, that's why it's False.")
 
+# This line prints some text
 print("How about some more.")
 
+# These lines print some text and then the result of some calculations
 print("Is it greater?", 5 > -2)
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
+
+## Numbers and Math symbols
+#
+# +: plus
+# -: minus
+# /: slash
+# *: asterisk
+# %: percent
+# <: less-than
+# >: greater-than
+# <=: less-than-equal
+# >=: greater-than-equal
