3b8fe154eb8468b3a85872d4c9a3939c1a46f19d
zero1nine <jeffrey.d.pyle@gmail.com>
2021-07-29 22:38:24 -0700

    added comments

--- ex15.py
+++ ex15.py
@@ -1,15 +1,23 @@
+# from the sys module, import argv
 from sys import argv
 
+# store arguments into two variables
 script, filename = argv
 
+# open file with name stored in "filename" into variable "txt"
+# open creates a "file object"
 txt = open(filename)
 
+# print the file name, and then read the opened file object and print it
 print(f"Here's your file {filename}:")
 print(txt.read())
 
+# prompt user to type file name again
 print("Type the filename again:")
 file_again = input("> ")
 
+# using file name by user input, open the file into "txt_again"
 txt_again = open(file_again)
 
+# read and then print the file object
 print(txt_again.read())
