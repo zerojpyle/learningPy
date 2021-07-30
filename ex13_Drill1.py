2e405956113ea98394384b818f642a0c41d33116
zero1nine <jeffrey.d.pyle@gmail.com>
2021-07-29 21:58:37 -0700

    initial version

---
+++ ex13_Drill1.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+script, fruit, veggie = argv
+print("Script name:", script)
+print("Your fruit is:", fruit)
+print("Your vegetable is:", veggie)
---
+++ ex13_Drill2.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, sport, band, hobby, person = argv
+
+print("Script name:", script)
+print("Your favorite person is:", person)
+print("Your favorite sport is:", sport)
+print("Your favorite band is:", band)
+print("Your favorite hobby is:", hobby)
