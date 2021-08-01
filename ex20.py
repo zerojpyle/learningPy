434f52d3ce9c9d2236ead62134e62a4182026db6
zero1nine <jeffrey.d.pyle@gmail.com>
2021-07-31 22:03:33 -0700

    added comments

--- ex20.py
+++ ex20.py
@@ -2,12 +2,15 @@ from sys import argv
 
 script, input_file = argv
 
+# function to print the entire file
 def print_all(f):
     print(f.read())
 
+# function to set the file object to the beginning
 def rewind(f):
     f.seek(0)
 
+# function to print a specific line
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
@@ -23,11 +26,14 @@ rewind(current_file)
 
 print("Let's print three lines:")
 
+# define the current_line and then pass it to print_a_line function
 current_line = 1
 print_a_line(current_line, current_file)
 
+# increase current_line and then print it
 current_line = current_line + 1
 print_a_line(current_line, current_file)
 
+# increase current_line and then print it
 current_line = current_line + 1
 print_a_line(current_line, current_file)
