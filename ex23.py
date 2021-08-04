7661f4111cf7adacaf3840bb9a7861588ec69575
zero1nine <jeffrey.d.pyle@gmail.com>
2021-08-03 21:57:56 -0700

    added missing blank line

--- ex23.py
+++ ex23.py
@@ -4,6 +4,7 @@ script, input_encoding, error = sys.argv
 
 def main(language_file, encoding, errors):
     line = language_file.readline()
+    
     if line:
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
