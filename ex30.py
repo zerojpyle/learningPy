73b6e52d13dd042ee3bb3bd5826724a6d83dc219
zero1nine <jeffrey.d.pyle@gmail.com>
2021-08-20 10:39:26 -0700

    added comments

--- ex30.py
+++ ex30.py
@@ -2,22 +2,32 @@ people = 30
 cars = 40
 trucks = 15
 
-
+# compare whether there are enough cars
 if cars > people:
+    # if there are more cars than people:
     print("We should take the cars.")
 elif cars < people:
+    # if there are fewer cars than people:
     print("We should not take the cars.")
 else:
+    # if neither are true, then:
     print("We can't decide.")
 
+# compare how many trucks vs. cars
 if trucks > cars:
+    # if there are more trucks than cars:
     print("That's too many trucks.")
 elif trucks < cars:
+    # if there are fewer trucks than cars:
     print("Maybe we could take the trucks.")
 else:
+    # if neither are true, then:
     print("We still can't decide.")
 
+# compare whether there are more people than trucks
 if people > trucks:
+    # if there are more people than trucks:
     print("Alright, let's just take the trucks.")
 else:
+    # if not, then:
     print("Fine, let's stay home then.")
