97267bf542107a32f8ebe2d47b7764a1f7ee3026
zero1nine <jeffrey.d.pyle@gmail.com>
2021-08-21 09:10:54 -0700

    added user inputs

--- ex33.py
+++ ex33.py
@@ -1,15 +1,22 @@
-i = 0
-numbers = []
+def loopy(mynum,inc):
+    i = 0
+    numbers = []
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+    while i < mynum:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottim i is {i}")
+        i = i + inc
+        print("Numbers now: ", numbers)
+        print(f"At the bottim i is {i}")
 
-print("The numbers: ")
+        print("The numbers: ")
 
-for num in numbers:
-    print(num)
+        for num in numbers:
+            print(num)
+
+print("Give me a number to count to:")
+number = int(input("> ")) + 1
+print("What should I count by?")
+increment = int(input("> "))
+loopy(number,increment)
